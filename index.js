0f3a8e66982217d9b1162315aefae1ef78a09925
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-03-20 09:24:00 +0100

    Initializing functionalities

---
+++ index.js
@@ -0,0 +1,11 @@
+const connectWallet = async () => {
+  console.log("Connecting ....")
+}
+
+const checkForMetaMask = async () => {
+  console.log("Ready")
+}
+
+const checkForAuthenticatedEthereumWallet = async () => {
+  console.log("yes")
+}  
